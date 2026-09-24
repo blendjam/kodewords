a3690f2d261be6536fd08c2470b20a864a4def82
blendjam <slamsal2000@gmail.com>
2026-09-24 23:26:47 +0545

    update: server deploy scripts

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -2,19 +2,25 @@ module.exports = {
   apps: [
     {
       name: "kodewords",
-      script: "/home/ubuntu/games/kodewords/server/dist/index.js",
+      script: "/home/ubuntu/games/kodewords/server/index.js",
       cwd: "/home/ubuntu/games/kodewords/server",
+
       instances: 1,
       exec_mode: "fork",
+
       autorestart: true,
       watch: false,
+
       max_memory_restart: "500M",
+
       env: {
         NODE_ENV: "production",
         PORT: 8080,
       },
+
       error_file: "/home/ubuntu/games/kodewords/logs/error.log",
       out_file: "/home/ubuntu/games/kodewords/logs/out.log",
+
       log_date_format: "YYYY-MM-DD HH:mm:ss Z",
       merge_logs: true,
     },
